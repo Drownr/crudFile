151501100a6ec61aa9ce798f966bfae0f87d3683
Drownr <33448811+Drownr@users.noreply.github.com>
2018-01-07 15:41:36 +0100

    Ordine 2

--- login/pannelloAdmin.php
+++
@@ -1,6 +0,0 @@
-<!-- Pannello amministratore -->
-<header>Pannello amministratore</header>
-<br><br>
-<input type='button' id="go" value='Elenca Utenti' onclick="cambiaStato(3);"/><br>
-<input type='button' id="go" value='Log-Out' onclick="cambiaStato(0);"/>
-<br><br>
--- login/pannelloUtente.php
+++
@@ -1,3 +0,0 @@
-<!-- Pannello utente -->
-<header>Area Utente</header><br>
-<input type='button' id="go" value='Log-Out' onclick="cambiaStato(0);"/><br><rb>
